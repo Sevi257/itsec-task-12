2c12199475704e40abe636c697fdc7ff395d321b
sevi1 <sevi1@web.de>
2023-11-20 09:54:22 +0100

    why not again

--- second_test.py
+++ second_test.py
@@ -32,56 +32,61 @@ print(len(iv))
 result = []
 # An die erste Stelle im result array kommt auch das erste byte also immer appenden
 found_same = -1
-for block in range(1, 4):
-    for i in range(16):
+blockcounter = 0
+for i in range(len(msg)):
+    if i == 16 or i == 32:
+        blockcounter += 1
+    if i > 48:
+        break
+    found = False
+    s = socket.socket()
+    s.connect(("itsec.sec.in.tum.de", 7023))
+    # s.connect(("localhost", 1024))
+    start = read_until(s, b"Do you")
+    ########################################
+    if i == 0:
+        pattern = re.compile(r'IV was (.+?)\)\n\n', re.DOTALL)
+        match = pattern.search(start.decode('utf-8'))
+        iv = binascii.unhexlify(match.group(1))
+        encrypted_message = start.split(b'\n')[1].decode('utf-8')
+        encrypted_message = encrypted_message.split("(")[0].strip()
+        bytes_cipher = binascii.unhexlify(encrypted_message)
+        msg = binascii.unhexlify(encrypted_message)
+
+    print(result)
+    for j in range(256):
         s = socket.socket()
         s.connect(("itsec.sec.in.tum.de", 7023))
         # s.connect(("localhost", 1024))
-        start = read_until(s, b"Do you")
-        ########################################
-        if i == 0:
-            pattern = re.compile(r'IV was (.+?)\)\n\n', re.DOTALL)
-            match = pattern.search(start.decode('utf-8'))
-            iv = binascii.unhexlify(match.group(1))
-            encrypted_message = start.split(b'\n')[1].decode('utf-8')
-            encrypted_message = encrypted_message.split("(")[0].strip()
-            bytes_cipher = binascii.unhexlify(encrypted_message)
-            msg = binascii.unhexlify(encrypted_message)
 
-        print(result)
-        for j in range(256):
-            s = socket.socket()
-            s.connect(("itsec.sec.in.tum.de", 7023))
-            # s.connect(("localhost", 1024))
+        read_until(s, b"Do you")
+        test_msg = bytearray(msg)
+        test_msg[-i - 17] ^= j
+        # Change test_msg slightly
 
-            read_until(s, b"Do you")
-            test_msg = bytearray(msg)
-            test_msg[-i - (17*block) ] ^= j
-            # Change test_msg slightly
+        for k in range(i):
+            new_byte = result[k] ^ (k + 1)
+            test_msg[-17 - k] ^= new_byte
+        final_msg = test_msg
+        if i >= 16:
+            final_msg = test_msg[:-16]
+        if i >= 32:
+            final_msg = test_msg[:-16]
 
-            for k in range(i):
-                new_byte = result[k + (block-1)*16] ^ (k + 1)
-                test_msg[(-17*block) - k] ^= new_byte
-            final_msg = test_msg
-            if block == 2:
-                final_msg = test_msg[:-16]
-            if block == 3:
-                final_msg = test_msg[:-32]
+        final_msg[len(test_msg)-i-17-1] = 0xFF
+        final_msg[len(test_msg)-i-17-2] = 0xFF
+        print(binascii.hexlify(final_msg))
 
-            final_msg[-i-17-1] = 0xFF
-            final_msg[-i-17-2] = 0xFF
-            print(binascii.hexlify(final_msg))
+        s.send(binascii.hexlify(iv) + b"\n")
+        s.send(binascii.hexlify(final_msg) + b"\n")
+        response = read_until(s, b"\n")
+        if "Bad" not in str(response):
+            og_message = (i + 1) ^ j
+            result.append(og_message)
 
-            s.send(binascii.hexlify(iv) + b"\n")
-            s.send(binascii.hexlify(final_msg) + b"\n")
-            response = read_until(s, b"\n")
-            if "Bad" not in str(response):
-                og_message = (i + 1) ^ j
-                result.append(og_message)
-
-                # Append the final result outside the loop
-                print("Succesful ", chr(og_message))
-                break
+            # Append the final result outside the loop
+            print("Succesful ", chr(og_message))
+            break
 flag = ""
 for char in result:
     flag = chr(char) + flag
